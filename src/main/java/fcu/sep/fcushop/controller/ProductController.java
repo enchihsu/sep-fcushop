38315b7826819e3d572127f39893a3b9a505ea87
enchihsu <D0886274@o365.fcu.edu.tw>
2021-12-25 21:36:41 +0800

    insert and update

--- src/main/java/fcu/sep/fcushop/controller/ProductController.java
+++ src/main/java/fcu/sep/fcushop/controller/ProductController.java
@@ -5,10 +5,8 @@ import fcu.sep.fcushop.model.Product;
 import fcu.sep.fcushop.service.ProductService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 /**
  *this is a class for get product information.
  */
@@ -28,4 +26,27 @@ public class ProductController {
   public List<Product> getProducts(@PathVariable("keyword") String keyword) {
     return productManager.getProducts(keyword);
   }
+
+  @RequestMapping(value="/add_product", method = RequestMethod.GET)
+  @ResponseBody
+  public String addProduct(
+      @RequestParam("book_name") String book_name,
+      @RequestParam("img_url") String img_url,
+      @RequestParam("price") Integer price,
+      @RequestParam("description") String description
+  )
+  {
+    return productManager.AddProduct(book_name, img_url, price, description);
+  }
+
+  @RequestMapping(value="/update_product", method = RequestMethod.GET)
+  @ResponseBody
+  public String updateProduct(
+      @RequestParam("book_name") String book_name,
+      @RequestParam("price") Integer price
+  )
+  {
+    return productManager.UpdateProduct(book_name, price);
+  }
+
 }
--- src/main/java/fcu/sep/fcushop/service/ProductService.java
+++ src/main/java/fcu/sep/fcushop/service/ProductService.java
@@ -36,4 +36,38 @@ public class ProductService {
         .executeAndFetch(Product.class);
     }
   }
+
+  public String AddProduct(String book_name, String img_url, int price, String description)
+  {
+    try (Connection connection = sql2oDbHandler.getConnector().open()) {
+      String query = "insert into fcu_shop.product (NAME, IMAGE_URL, PRICE, DESCRIPTION) "
+          + "VALUES(:book_name, :img_url, :price, :description)";
+
+      System.out.println(query);
+      connection.createQuery(query)
+          .addParameter("book_name", book_name)
+          .addParameter("img_url", img_url)
+          .addParameter("price", price)
+          .addParameter("description", description)
+          .executeUpdate();
+      return "Success";
+    }
+  }
+
+  public String UpdateProduct(String book_name, int price)
+  {
+    try (Connection connection = sql2oDbHandler.getConnector().open()) {
+      String query = "Update fcu_shop.product "
+          + "SET PRICE= :price WHERE NAME = :book_name";
+
+      System.out.println(query);
+      connection.createQuery(query)
+          .addParameter("book_name", book_name)
+          .addParameter("price", price)
+          .executeUpdate();
+      return "Success";
+
+    }
+  }
+
 }
